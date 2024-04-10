95c6bd355f4544e36c628f50ad12f07f3511b886
Tyler Tierney <tytierney@yahoo.com>
2024-04-10 19:13:20 -0400

    add reply_count to /posts endpoint

--- src/api/posts/posts.ts
+++ src/api/posts/posts.ts
@@ -27,16 +27,23 @@ posts.get("/", (req, res, next) => {
       posts.photo_url,
       posts.replying_to,
       COALESCE(ARRAY_AGG(tags.text) FILTER (WHERE tags.text IS NOT NULL), '{}') tags,
-      COALESCE(ARRAY_AGG(DISTINCT likes.user_id) FILTER (WHERE likes.user_id IS NOT NULL), '{}') likes
+      COALESCE(ARRAY_AGG(DISTINCT likes.user_id) FILTER (WHERE likes.user_id IS NOT NULL), '{}') likes,
+      COALESCE(reply_counts.replies_count, 0) AS reply_count
     FROM posts
       JOIN users
         ON users.id=posts.author
       LEFT JOIN tags
         ON posts.id=tags.post_id
       LEFT JOIN likes ON posts.id = likes.post_id
+      LEFT JOIN (
+        SELECT replying_to, COUNT(*)::int as replies_count
+        FROM posts
+        GROUP BY posts.id
+      ) reply_counts ON posts.id=reply_counts.replying_to
     GROUP BY
       posts.id,
-      users.id
+      users.id,
+      reply_count
     ORDER BY date DESC;`;
 
   query(text, [], (error, result) => {
