623e480f303e04fcdffa6d7597f966679d6fd9af
Tyler Tierney <tytierney@yahoo.com>
2024-04-10 19:42:39 -0400

    add replies and likes when fetching individual post

--- src/api/posts/posts.ts
+++ src/api/posts/posts.ts
@@ -12,9 +12,6 @@ posts.use(token);
 posts.use("/like", like);
 
 posts.get("/", (req, res, next) => {
-  console.log(req.headers.USERNAME);
-  console.log(req.headers.USER_ID);
-
   const text = `
     SELECT
       date,
@@ -96,22 +93,34 @@ posts.get("/:userId/feed", (req, res) => {
 
 posts.get("/:post_id", (req, res) => {
   const text = `
-  SELECT
-    name,
-    date,
-    posts.text,
-    username,
-    posts.photo_url,
-    author,
-    profile_pic,
-    replying_to,
-    posts.id AS id,
-    COALESCE(ARRAY_AGG(tags.text) FILTER (WHERE tags.text IS NOT NULL), '{}') tags
-  FROM users
-    JOIN posts ON users.id=posts.author
-    LEFT JOIN tags ON posts.id=tags.post_id
-  WHERE posts.id=$1
-  GROUP BY posts.id, users.id;`;
+    SELECT
+      date,
+      posts.text,
+      author,
+      username,
+      name,
+      profile_pic,
+      posts.id AS id,
+      posts.photo_url,
+      posts.replying_to,
+      COALESCE(ARRAY_AGG(tags.text) FILTER (WHERE tags.text IS NOT NULL), '{}') tags,
+      COALESCE(ARRAY_AGG(DISTINCT likes.user_id) FILTER (WHERE likes.user_id IS NOT NULL), '{}') likes,
+      COALESCE(reply_counts.replies_count, 0) AS reply_count
+    FROM posts
+      JOIN users ON users.id=posts.author
+      LEFT JOIN tags ON posts.id=tags.post_id
+      LEFT JOIN likes ON posts.id = likes.post_id
+      LEFT JOIN (
+        SELECT replying_to, COUNT(*)::int as replies_count
+        FROM posts
+        GROUP BY posts.id
+      ) reply_counts ON posts.id=reply_counts.replying_to
+    WHERE posts.id=$1
+    GROUP BY
+      posts.id,
+      users.id,
+      reply_count
+    ORDER BY date DESC;`;
 
   query(text, [req.params.post_id], (error, result) => {
     if (error) return res.status(401).json(error);
